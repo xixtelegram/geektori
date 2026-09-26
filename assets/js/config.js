a0b928ced52e7a06dd1f88e4a522bce433db5fa1
xixtelegram <xixtelegram@gmail.com>
2026-09-26 13:43:35 +0330

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-/** تنظیمات مشترک پروژه */
-export const WORKER_URL = 'https://billowing-flower-c4be.xixtelegram.workers.dev';
-
-export const GITHUB = {
-  owner: 'xixtelegram',
-  repo: 'geektori',
-  branch: 'main',
-};
--- assets/js/dashboard/state.js
+++
@@ -1,7 +0,0 @@
-export const state = {
-  rawRows: [],
-  products: [],
-  currentView: 'grouped',
-  chartQty: null,
-  chartRev: null,
-};
